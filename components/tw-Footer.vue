3dfbcb987e75ad6aea4126e437e39cea24efc657
Rob van Eijk <rob@blaeu.com>
2024-01-20 12:34:22 +0100

    test 12:33

--- components/tw-Footer.vue
+++ components/tw-Footer.vue
@@ -3,7 +3,7 @@
         <div class="container mx-auto">
             <div class="flex flex-col justify-center items-center">
                 <div class="w-full text-left">
-				<b>Copyright &copy; 2024 Rob van Eijk (CC BY 4.0).</b>
+				<b>Copyright &copy; 2024 Rob van Eijk (CC BY 4.0)</b>
                 </div>
                 <div class="w-full text-left text-xs">
 				 <br /><b>Disclaimer:</b> My opinions are my own and do not reflect the views of my employer or any affiliated organizations. <b>Privacy Statement:</b> This page (<a href="https://github.com/rvaneijk/blaeu.com" target="_blank" alt="code blaeu.com" title="code blaeu.com">source code <i class="fa-solid fa-external-link fa-xs"></i></a>) is free from (tracking) cookies or similar technologies. We delete access logs after 3 months. <b>Responsible Disclosure:</b> If you have found a vulnerability we would like to hear about it so we can take action as soon as possible. See our <a href="https://blaeu.com/.well-known/responsible_disclosure_policy.txt" target="_blank" alt="responsible_disclosure_policy.txt" title="responsible_disclosure_policy.txt">Responsible Disclosure Policy <i class="fa-solid fa-external-link fa-xs"></i></a> and <a href="https://blaeu.com/.well-known/security.txt" target="_blank" alt="security.txt" title="security.txt">security.txt <i class="fa-solid fa-external-link fa-xs"></i></a> at the well-known location. <b>All rights reserved:</b> Includes Amble Light (Apache License 2.0), Big John (SIL OFL 1.1), Font Awesome Free (CC BY 4.0), Node.js components (Node.js contributors' terms), Nuxt.js components (MIT), Tailwind CSS (MIT), and TW Elements (GNU AGPL v3). 
