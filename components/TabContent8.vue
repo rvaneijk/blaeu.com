832f2a509c4dcdd6237f1399c3d5bf451e9f6317
Rob van Eijk <rob@blaeu.com>
2024-05-29 14:14:11 +0200

    test 14:14

--- components/TabContent8.vue
+++ components/TabContent8.vue
@@ -228,9 +228,18 @@ href="https://www.dcu.ie/courses/postgraduate/school-law-and-government/ma-data-
                     <span class="text-red-600 font-semibold">E: CONTRIBUTIONS TO PROFESSIONAL LEARNING PROGRAMS</span>
                     </li>
                     <li class="mb-2">
-                      <span class="text-red-600 font-semibold">2024 (1)</span>
+                      <span class="text-red-600 font-semibold">2024 (2)</span>
                     </li>
                     <li class="mb-2">Eijk R. van (2024). <i>Connecting Privacy-Enhancing Technologies (PETs) with Digital Advertising: Highlighting the Significance of Introducing Noise into Data Flows.</i> Singapore: Personal Data Protection Commission Singapore, Infocomm Media Development Authority.<br /> <span class="text-red-600 font-semibold">professional learning</span></li>
+                    <li class="mb-2">
+                      <a
+href="https://fpf.org/training/"
+                        target="_blank" title="Click to visit external webpage">
+                        Eijk R. van (2024). De-Identification. In <i>FPF Training Program.</i> Washington, DC: Future
+                        of Privacy Forum.
+                        <i class="fa-solid fa-external-link fa-sm"></i></a><br />
+                      <span class="text-red-600 font-semibold">professional learning</span>
+                    </li>
                     <li class="mb-2">
 					<br />
                       <span class="text-red-600 font-semibold">2023 (6)</span>
